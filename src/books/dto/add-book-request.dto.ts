a8e77bbc13e8fdbe1a3c51ba446a13de8740cbf6
volodymyr <vvovk225@gmail.com>
2026-02-15 20:57:09 +0200

    feat/improved validation

--- src/books/dto/add-book-request.dto.ts
+++ src/books/dto/add-book-request.dto.ts
@@ -1,6 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
 export class AddBookByISBNRequestDto {
   @IsString()
+  @Matches(/^(?:\d{10}|\d{13})$/, {
+    message: 'ISBN must be either 10 or 13 digits',
+  })
   isbn: string;
 }
