b8ef63445fc3c70b62f879c9aa28cfb9a09b6f74
volodymyr <vvovk225@gmail.com>
2026-02-15 17:08:15 +0200

    test deploy

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -2,13 +2,17 @@ import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { GetBookByISBNRequestDto } from './dto/get-book-by-isbn-request.dto';
 import { GetBookByISBNResponseDto } from './dto/get-book-by-isbn-response.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Controller({
   path: 'books',
   version: '1',
 })
 export class BooksController {
-  constructor(private readonly booksService: BooksService) {}
+  constructor(
+    private readonly booksService: BooksService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
   async getBookByISBN(@Query() dto: GetBookByISBNRequestDto) {
@@ -19,5 +23,10 @@ export class BooksController {
     }
 
     return new GetBookByISBNResponseDto(book.items[0]);
+  }
+
+  @Get('users')
+  async getUsers() {
+    return await this.prisma.profile.findMany();
   }
 }
