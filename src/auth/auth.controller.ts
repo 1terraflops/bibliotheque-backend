9400b90209a86f4503b87d4de2ec72da63eb79e5
volodymyr <vvovk225@gmail.com>
2026-02-18 18:53:21 +0200

    feat: added docs for auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,8 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GetTokenDto } from './dto/get-token';
 import { Public } from 'src/decorators/public.decorator';
+import {
+  ApiCreatedResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Public()
+@ApiTags('auth')
 @Controller({
   path: 'auth',
   version: '1',
@@ -12,6 +18,12 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('get-token')
+  @ApiCreatedResponse({
+    description: 'Logged in successfully',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid credentials',
+  })
   getToken(@Body() dto: GetTokenDto) {
     return this.authService.getToken(dto);
   }
--- src/auth/dto/get-token.ts
+++ src/auth/dto/get-token.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, Length } from 'class-validator';
 
 export class GetTokenDto {
+  @ApiProperty({
+    description: "User's email",
+    example: 'email@mail.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: "User's password",
+    example: '12345678',
+  })
   @IsString()
   @Length(6, 100)
   password: string;
